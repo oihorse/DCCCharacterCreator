0a3adac7d9ced09063eb69983c08f64d48f7170c
chris <chris.gohlinghorst@sixactualsoftware.com>
2015-07-12 17:57:33 -0400

    Basic buildview complete.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,25 @@ module.exports = function (grunt) {
                     watch: ['server.js', 'config/**/*.js', 'app/**/*.js']
                 }
             }
+        },
+        jshint: {
+            all: {
+                src: ['server.js', 'config/**/*.js', 'app/**/*.js', 'public/js/*.js', 'public/modules/**/*.js']
+            }
+        },
+        csslint: {
+            all: {
+                src: 'public/modules/**/*.css'
+            }
         }
     });
     grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+
+
+
     grunt.registerTask('default', ['env:dev']);
+    grunt.registerTask('lint', ['jshint', 'csslint']);
 };
